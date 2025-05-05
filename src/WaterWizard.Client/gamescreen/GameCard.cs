e4c45ac5a56d3b8f78d7f26f5d4199d51370e4bd
maxk2807 <max.kondratov@student.hs-rm.de>
2025-05-05 12:51:18 +0200

    done

--- src/WaterWizard.Client/gamescreen/GameCard.cs
+++ src/WaterWizard.Client/gamescreen/GameCard.cs
@@ -23,7 +23,35 @@ public class GameCard(GameScreen gameScreen, Cards card)
         Rectangle card = new(x, y, Width, Height);
         Raylib.DrawRectangleRec(card, GetColorFromCardType(front));
         Raylib.DrawRectangleLinesEx(card, 2, Color.Black);
+        if(!front){
+            string cardBackText = this.card.Type.ToString();
+            int textWidth = Raylib.MeasureText(cardBackText, 10);
+            Raylib.DrawText(cardBackText, x+(Width-textWidth)/2, y+(Height-10)/2, 10, Color.Black);
+        }
     }
+
+    /// <summary>
+    /// Draw the individual Card at the given coordinates with the given Rotation rot.
+    /// The Card currently consists of the given color, and a black outline.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="front"></param>
+    /// <param name="color"></param>
+    /// <param name="rot"></param>
+    public void DrawRotation(int x, int y, bool front, float rot)
+    {
+        Rectangle card = new(x, y, Width, Height);
+        Raylib.DrawRectanglePro(card, new(Width, Height), rot, GetColorFromCardType(front));
+        Raylib.DrawRectangleLinesEx(card, 2, Color.Black);
+        if(!front){
+            string cardBackText = this.card.Type.ToString();
+            int textWidth = Raylib.MeasureText(cardBackText, 10);
+            Raylib.DrawTextPro(Raylib.GetFontDefault(), cardBackText, new(x+(Width-textWidth)/2,y+(Height-10)/2), new(textWidth, 10), rot, 10, Raylib.GetFontDefault().GlyphPadding, Color.Black);
+        }
+    }
+
+
 
     private Color GetColorFromCardType(bool front){
         return front 
