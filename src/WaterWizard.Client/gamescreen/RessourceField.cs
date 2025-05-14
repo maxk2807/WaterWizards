222255e0baf748176a923ae22ebdcf1d658f4220
maxk2807 <max.kondratov@student.hs-rm.de>
2025-05-14 13:41:35 +0200

    Mana und Gold Anzeige

---
+++ src/WaterWizard.Client/gamescreen/RessourceField.cs
@@ -0,0 +1,54 @@
+using Raylib_cs;
+
+namespace WaterWizard.Client.gamescreen;
+
+public class RessourceField(GameScreen gameScreen)
+{
+    private float Mana;
+    private string manaString = "";
+    private float Gold;
+    private string goldString = "";
+
+    private int X;
+    private int Y;
+    private int Width;
+    private int Height;
+    private int CellSize => gameScreen.playerBoard!.CellSize;
+    private int ZonePadding => (int)gameScreen.ZonePadding;
+
+    private Rectangle GoldRec;
+    private Rectangle ManaRec;
+
+    public void Initialize()
+    {
+        Mana = 4f;
+        manaString = Mana.ToString() + " Mana";
+        Gold = 5f;
+        goldString = Gold.ToString() + " $";
+
+        X = ZonePadding;
+        Y = ZonePadding * 2 + 20; //20 is fontsize of GameTimer in GameScreen
+        Width = (int)gameScreen.opponentBoard!.Position.X - ZonePadding * 2;
+        Height = gameScreen.cardStacksField!.Y - 20 - ZonePadding * 3;
+
+        int goldFontSize = 30;
+        int goldWidth = Raylib.MeasureText(goldString, goldFontSize);
+        int goldX = X + (int)(Width / 4f - goldWidth / 2f);
+        int goldY = (int)(Y + (Height - goldFontSize) / 2f);
+
+        GoldRec = new(goldX, goldY, goldWidth, goldFontSize);
+
+        int manaFontSize = 30;
+        int manaWidth = Raylib.MeasureText(manaString, manaFontSize);
+        int manaX = X + (int)(3 * Width / 4f - manaWidth / 2d);
+        int manaY = (int)(Y + (Height - manaFontSize) / 2f);
+
+        ManaRec = new(manaX, manaY, manaWidth, manaFontSize);
+    }
+
+    public void Draw()
+    {
+        Raylib.DrawText(goldString, (int)GoldRec.X, (int)GoldRec.Y, (int)GoldRec.Height, Color.Black);
+        Raylib.DrawText(manaString, (int)ManaRec.X, (int)ManaRec.Y, (int)ManaRec.Height, Color.Black);
+    }
+}
