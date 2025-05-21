29cc8e1c5a75d56a2d4425c5958923916ca3e153
maxk2807 <max.kondratov@student.hs-rm.de>
2025-05-21 13:48:01 +0200

    Documentation and Exception Type

--- src/WaterWizard.Server/GameState.cs
+++ src/WaterWizard.Server/GameState.cs
@@ -62,10 +62,10 @@ public class GameState
     public List<Cards> Graveyard { get; private set; }
 
     /// <summary>
-    /// Represents the GameState
+    /// A new Gamestate.
     /// </summary>
-    /// <param name="server"></param>
-    /// <param name="manager"></param>
+    /// <param name="server">Corresponding NetManager server to access Clients (Players)</param>
+    /// <param name="manager">Corresponding <see cref="ServerGameStateManager"/></param>
     /// <exception cref="InvalidOperationException"></exception>
     public GameState(NetManager server, ServerGameStateManager manager)
     {
@@ -89,6 +89,13 @@ public class GameState
         this.manager = manager;
     }
 
+    /// <summary>
+    /// Initializes the Boards
+    /// </summary>
+    /// <returns>
+    /// 3D Cell Array where the First Dimension is a 2 Element Array
+    /// where the Elements correspond to the 2 Players 
+    /// </returns>
     private static Cell[][,] InitBoards()
     {
         Cell[,] player1Board = new Cell[boardWidth, boardHeight];
@@ -104,6 +111,12 @@ public class GameState
         return [player1Board, player2Board];
     }
 
+    /// <summary>
+    /// Handles the Placement of the ships. Receives the Position of the ship placement 
+    /// from the Client. //TODO: Move Placement Validation from Client to here. 
+    /// </summary>
+    /// <param name="peer">The <see cref="NetPeer"/> Client sending the Placement Request</param>
+    /// <param name="reader"><see cref="NetPacketReader"/> with the Request Data</param>
     public void HandleShipPlacement(NetPeer peer, NetPacketReader reader)
     {
         int x = reader.GetInt();
@@ -138,7 +151,14 @@ public class GameState
         Console.WriteLine("[Server] Successfull Ship Handling");
     }
 
-    internal void HandleCardBuying(NetPeer peer, NetPacketReader reader)
+    /// <summary>
+    /// Handles the Buying of Cards from a CardStack. Takes a random Card from the corresponding CardStack
+    /// of the <see cref="CardType"/> given in <paramref name="reader"/>
+    /// </summary>
+    /// <param name="peer">The <see cref="NetPeer"/> Client sending the Placement Request</param>
+    /// <param name="reader"><see cref="NetPacketReader"/> with the Request Data</param>
+    /// <exception cref="UnknowCardTypeException"></exception>
+    public void HandleCardBuying(NetPeer peer, NetPacketReader reader)
     {
         string cardType = reader.GetString();
         Console.WriteLine($"[Server] Trying to Buy {cardType} Card");
@@ -147,7 +167,7 @@ public class GameState
             "Utility" => RandomCard(UtilityStack),//TODO: Actually Paying
             "Damage" => RandomCard(DamageStack),//TODO: Actually Paying
             "Environment" => RandomCard(EnvironmentStack),//TODO: Actually Paying
-            _ => throw new Exception("Invalid CardType: " + cardType + " . Has to be a string of either: Utility, Damage or Environment"),
+            _ => throw new UnknownCardTypeException("Invalid CardType: " + cardType + " . Has to be a string of either: Utility, Damage or Environment"),
         };
         NetDataWriter writer = new();
         writer.Put("BoughtCard");
@@ -164,12 +184,23 @@ public class GameState
         Console.WriteLine($"[Server] Player_{peer.Port} Bought Card {card.Variant}");
     }
 
+    /// <summary>
+    /// Utility method that gets a random card from the given cardstack
+    /// </summary>
+    /// <param name="stack">The cardstack that the random Card is taken from </param>
+    /// <returns>The random Card</returns>
     private static Cards RandomCard(List<Cards> stack)
     {
         var index = (int)(stack.Count * Random.Shared.NextSingle());
         return stack[index];
     }
 
+    /// <summary>
+    /// Handles the Casting of Cards from the <see cref="NetPeer"/>s hand. //TODO: handle Mana Cost.
+    /// Calls the Ability in <see cref="CardAbilities"/>
+    /// </summary>
+    /// <param name="peer">The <see cref="NetPeer"/> Client sending the Placement Request</param>
+    /// <param name="reader"><see cref="NetPacketReader"/> with the Request Data</param>
     public void HandleCardCasting(NetPeer peer, NetPacketReader reader)
     {
         //TODO: Handle Mana Cost
