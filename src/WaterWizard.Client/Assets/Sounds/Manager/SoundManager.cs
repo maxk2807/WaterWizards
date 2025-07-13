49e0bf6454fac3141b7358a6cc36fd3e02d14de9
maxk2807 <max.kondratov@student.hs-rm.de>
2025-07-13 18:02:57 +0200

    Explosion on Hit

--- src/WaterWizard.Client/Assets/Sounds/Manager/SoundManager.cs
+++ src/WaterWizard.Client/Assets/Sounds/Manager/SoundManager.cs
@@ -5,16 +5,26 @@ public static class SoundManager
 {
     public static Sound CardSound;
     public static Sound ButtonSound;
+    public static List<Sound> Explosions { get; private set; } = []; 
 
     public static void LoadSounds()
     {
         CardSound = Raylib.LoadSound("src/WaterWizard.Client/Assets/Sounds/DrawCard.wav");
         ButtonSound = Raylib.LoadSound("src/WaterWizard.Client/Assets/Sounds/ButtonClick.wav");
+        Explosions.Add(Raylib.LoadSound("src/WaterWizard.Client/Assets/Sounds/Explosions/explosion1.wav"));
+        Explosions.Add(Raylib.LoadSound("src/WaterWizard.Client/Assets/Sounds/Explosions/explosion2.wav"));
     }
 
     public static void UnloadSounds()
     {
         Raylib.UnloadSound(CardSound);
         Raylib.UnloadSound(ButtonSound);
+        Explosions.ForEach(Raylib.UnloadSound);
+    }
+
+    public static Sound RandomExplosion()
+    {
+        int i = new Random().Next(0, Explosions.Count);
+        return Explosions[i];
     }
 }
