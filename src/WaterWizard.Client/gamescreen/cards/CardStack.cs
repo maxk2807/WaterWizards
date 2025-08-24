6a09a67551a04917d2c3f0d7d9f24490291f6006
jlnhsrm <julian.ramscheid@student.hs-rm.de>
2025-08-24 17:36:32 +0200

    CardStack - docu

--- src/WaterWizard.Client/gamescreen/cards/CardStack.cs
+++ src/WaterWizard.Client/gamescreen/cards/CardStack.cs
@@ -15,19 +15,38 @@ using Raylib_cs;
 
 namespace WaterWizard.Client.gamescreen.cards;
 
+/// <summary>
+/// Stellt einen Kartenstapel im Spielfeld dar.
+/// Der Stapel kann mit verschiedenen Kartentypen (Schaden, Heilung, Utility, Umgebung) 
+/// initialisiert werden und rendert entweder die oberste Karte oder 
+/// ein Platzhalter-Rechteck, wenn der Stapel leer ist.
+/// </summary>
 public class CardStack(GameScreen gameScreen, int x, int y)
 {
+    /// <summary>
+    /// X-Position des Kartenstapels auf dem Bildschirm.
+    /// </summary>
     public int X { get; private set; } = x;
+
+    /// <summary>
+    /// Y-Position des Kartenstapels auf dem Bildschirm.
+    /// </summary>
     public int Y { get; private set; } = y;
     private List<GameCard> cards = [];
 
+    /// <summary>
+    /// Y-Position des Kartenstapels auf dem Bildschirm.
+    /// </summary>
     public void InitDamage()
     {
         //later calls to Network to get correct Cards
         cards.Add(new(gameScreen, new(Shared.CardVariant.Firebolt)));
         cards.Add(new(gameScreen, new(Shared.CardVariant.GreedHit)));
     }
-
+    /// <summary>
+    /// Initialisiert den Stapel mit Utility-Karten 
+    /// (z. B. Kontroll- oder Verteidigungsfähigkeiten).
+    /// </summary>
     public void InitUtility()
     {
         //later calls to Network to get correct Cards
@@ -35,18 +54,30 @@ public class CardStack(GameScreen gameScreen, int x, int y)
         cards.Add(new(gameScreen, new(Shared.CardVariant.Shield)));
     }
 
+    /// <summary>
+    /// Initialisiert den Stapel mit Umgebungskarten 
+    /// (z. B. Wetter- oder Feld-Effekten).
+    /// </summary>
     public void InitEnvironment()
     {
         //later calls to Network to get correct Cards
         cards.Add(new(gameScreen, new(Shared.CardVariant.Thunder)));
     }
 
+    /// <summary>
+    /// Initialisiert den Stapel mit Heilungs-Karten.
+    /// </summary>
     public void InitHealing()
     {
         //later calls to Network to get correct Cards
         cards.Add(new(gameScreen, new(Shared.CardVariant.Heal)));
     }
 
+    /// <summary>
+    /// Zeichnet den Stapel. 
+    /// Wenn Karten vorhanden sind, wird die oberste Karte dargestellt.
+    /// Andernfalls wird ein Platzhalter-Rechteck angezeigt.
+    /// </summary>
     public void Draw()
     {
         if (cards.Count == 0)
